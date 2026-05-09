99bf74e846ca00783160673c112786e08f796a18
destradag <destradag@unal.edu.co>
2026-05-09 09:06:02 -0500

    Submitting

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,7 +3,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
-
+import glob
+import os
+import pandas as pd
 
 def pregunta_01():
     """
@@ -18,3 +20,34 @@ def pregunta_01():
 
 
     """
+    path = os.path.join("files/input", "clusters_report.txt")
+    dataframe = pd.read_fwf(
+        path,
+        engine='python',
+        skiprows=4,
+        names=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    )
+    
+    # Normalize keywords: ensure single space between words, comma separated
+    dataframe = dataframe.copy()
+    dataframe['principales_palabras_clave'] = dataframe['principales_palabras_clave'].str.replace(r'\s+', ' ', regex=True).str.strip()
+    dataframe = dataframe.copy()
+    columnas_fijas = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave']
+    dataframe[columnas_fijas] = dataframe[columnas_fijas].ffill()
+    dataframe = dataframe.copy()
+    dataframe = dataframe.groupby(columnas_fijas, as_index=False).agg({'principales_palabras_clave':' '.join})
+    dataframe = dataframe.copy()
+    dataframe['porcentaje_de_palabras_clave'] = dataframe['porcentaje_de_palabras_clave'].apply(lambda x: str(x).replace(' %',''))
+    dataframe = dataframe.copy()
+    dataframe['porcentaje_de_palabras_clave'] = dataframe['porcentaje_de_palabras_clave'].apply(lambda x: float(str(x).replace(',','.')))
+    dataframe = dataframe.copy()
+    dataframe['cluster'] = dataframe['cluster'].apply(lambda x: int(float((str(x)))))
+    dataframe = dataframe.copy()
+    dataframe['cantidad_de_palabras_clave'] = dataframe['cantidad_de_palabras_clave'].apply(lambda x: int(float((str(x)))))
+    dataframe = dataframe.copy()
+    dataframe['principales_palabras_clave'] = dataframe['principales_palabras_clave'].apply(lambda x: str(x).replace('.',''))
+
+    return dataframe
+
+if __name__ == '__main__':
+   print(pregunta_01())
